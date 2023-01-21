7014158ced2062d1f35891f6ec4bf2b5e39b25f8
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2023-01-21 18:16:31 +0530

    Submitting Q11 of Assignment 2.

---
+++ a11_q11_A2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+    
+    //write your code here
+    int count;
+    cin>>count;
+            
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+           
+            cout<<count;
+            ++count;
+            
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+}
